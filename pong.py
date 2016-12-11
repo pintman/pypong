f861862c8fd8efc1545e23d5933d2f9729e5dd58
Marco Bakera <bakera@tbs1.de>
2016-12-11 16:08:27 +0100

    init commit

---
+++ pong.py
@@ -0,0 +1,35 @@
+import threading
+import tkinter
+
+class Schlaeger:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.rect = self.canvas.create_rectangle(10,200,20,200-50,
+                                                 fill="black")
+
+    def hoch(self):
+        self.canvas.move(self.rect, 0, -10) # move rectangle up
+        
+    def runter(self):
+        self.canvas.move(self.rect, 0, +10) # move rectangle down
+
+class App:
+    def __init__(self):
+        self.fenster = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.fenster)
+        self.canvas.pack()
+
+        self.schlaeger = Schlaeger(self.canvas)
+
+        btn = tkinter.Button(self.fenster, text="^", command=self.btn_up_click)
+        btn.pack()        
+        btn = tkinter.Button(self.fenster, text="v", command=self.schlaeger.runter)
+        btn.pack()        
+
+        self.fenster.mainloop()
+
+    def btn_up_click(self):
+        self.schlaeger.hoch()
+        
+app = App()
+
